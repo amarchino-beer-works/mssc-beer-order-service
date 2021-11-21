b993319cd47d9c6dfe5a1b8299885746686966a0
amarchino <marchino.alessandro@gmail.com>
2021-11-21 23:13:14 +0100

    Lesson 137

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceRestTemplateImpl.java
@@ -0,0 +1,35 @@
+package guru.sfg.beer.order.service.services.beer;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@Component
+@RequiredArgsConstructor
+public class BeerServiceRestTemplateImpl implements BeerService {
+	private static final String BEER_PATH_V1 = "/api/v1/beer/{id}";
+	private static final String BEER_UPC_PATH_V1 = "/api/v1/beerUpc/{upc}";
+	private final RestTemplate restTemplate;
+	
+	@Setter
+	private String beerServiceHost;
+
+	@Override
+	public Optional<BeerDto> getBeerById(UUID id) {
+		return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_PATH_V1, BeerDto.class, id));
+	}
+
+	@Override
+	public Optional<BeerDto> getBeerByUpc(String upc) {
+		return Optional.of(restTemplate.getForObject(beerServiceHost + BEER_UPC_PATH_V1, BeerDto.class, upc));
+	}
+
+}
---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerDto.java
@@ -0,0 +1,37 @@
+package guru.sfg.beer.order.service.web.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+	private static final long serialVersionUID = 3545736275716912663L;
+	private UUID id;
+	private Integer version;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = Shape.STRING)
+	private OffsetDateTime createdDate;
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = Shape.STRING)
+	private OffsetDateTime lastModifiedDate;
+	
+	private String beerName;
+	private String beerStyle;
+	private String upc;
+	
+	@JsonFormat(shape = Shape.STRING)
+	private BigDecimal price;
+	private Integer quantityOnHand;
+}
