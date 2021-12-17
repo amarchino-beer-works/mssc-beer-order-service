502e47b951db904a4b310f6c6a6d9987ec5fb1de
amarchino <marchino.alessandro@gmail.com>
2021-12-17 13:59:46 +0100

    Lesson 235

---
+++ src/test/java/guru/sfg/beer/order/service/services/BeerOrderManagerImplIT.java
@@ -0,0 +1,54 @@
+package guru.sfg.beer.order.service.services;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.domain.Customer;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.repositories.CustomerRepository;
+
+@SpringBootTest
+public class BeerOrderManagerImplIT {
+
+	@Autowired BeerOrderManager beerOrderManager;
+	@Autowired BeerOrderRepository beerOrderRepository;
+	@Autowired CustomerRepository customerRepository;
+	
+	Customer testCustomer;
+	UUID beerId = UUID.randomUUID();
+	
+	@BeforeEach
+	void setUp() {
+		testCustomer = customerRepository.save(Customer.builder().customerName("Test customer").build());
+	}
+	
+	@Test
+	void testNewToAllocate() {
+		BeerOrder beerOrder = createBeerOrder();
+		BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
+		Assertions.assertNotNull(savedBeerOrder);
+		Assertions.assertEquals(BeerOrderStatusEnum.ALLOCATED, savedBeerOrder.getOrderStatus());
+	}
+	
+	BeerOrder createBeerOrder() {
+		BeerOrder beerOrder = BeerOrder.builder().customer(testCustomer).build();
+		Set<BeerOrderLine> lines = new HashSet<>();
+		lines.add(BeerOrderLine.builder().beerId(beerId).orderQuantity(1).beerOrder(beerOrder).build());
+		beerOrder.setBeerOrderLines(lines);
+		return beerOrder;
+	}
+}
+
+
+
+
